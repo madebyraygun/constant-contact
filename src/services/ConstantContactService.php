e681b6775b60eab300b372b4b597b7917b5828ed
Dalton Rooney <dalton@madebyraygun.com>
2018-12-06 11:57:13 -0800

    New addContact and updateContact methods
    subscribe method now checks for contact existence before trying to add a new subscriber.

--- src/services/ConstantContactService.php
+++ src/services/ConstantContactService.php
@@ -41,6 +41,41 @@ class ConstantContactService extends Component
         $plugin = Plugin::getInstance();
         $settings = $plugin->getSettings();
         $client = new Client($settings->key, $settings->token);
+        
+        $options = [
+            'query' => [
+                'email' => $email
+            ]
+        ];
+
+        try {
+            $response = $client->request('GET', 'contacts', $options);
+         } catch (\Exception $e) {
+            return [
+                'success' => false,
+                'message' => 'An unknown error occurred.'
+            ];
+        }
+
+        $responseObj = json_decode($response->getBody()->getContents());
+        if ( !empty($responseObj->results) ) {
+            return $this->updateContact($responseObj->results, $listID);
+        } else {
+            return $this->addContact($email, $listID);
+        }
+
+        return null;
+    }
+
+    /**
+     *
+     * @return mixed
+     */
+    private function addContact($email, $listID) {
+        $plugin = Plugin::getInstance();
+        $settings = $plugin->getSettings();
+        $client = new Client($settings->key, $settings->token);
+
         $payload = [
             'lists' => [
                 ['id' => $listID]
@@ -50,10 +85,8 @@ class ConstantContactService extends Component
             ]
         ];
 
-        $action = 'ACTION_BY_VISITOR';
-
         try {
-            $response = $client->addContact($payload, $action);
+            $response = $client->addContact($payload, 'ACTION_BY_VISITOR');
             return [
                 'success' => true,
                 'message' => "You've been added to the list."
@@ -71,5 +104,51 @@ class ConstantContactService extends Component
                 'message' => 'An unknown error occurred.'
             ];
         }
+        return null;
+    }
+
+    /**
+     *
+     * @return mixed
+     */
+    private function updateContact($contact, $listID) {
+        $plugin = Plugin::getInstance();
+        $settings = $plugin->getSettings();
+        $client = new Client($settings->key, $settings->token);
+
+        $contact = reset($contact);
+        $lists = $contact->lists;
+
+        foreach ( $lists as $list ) {
+            if ( $list->id == $listID ) {
+                return [
+                    'success' => false,
+                    'message' => 'You\'re already subscribed to this list.'
+                ];
+            }
+        }
+
+        $contact->lists[] = (object) array('id'=>$listID,'status'=>'ACTIVE');
+
+        try {
+            $response = $client->updateContact($contact, 'ACTION_BY_VISITOR');
+            return [
+                'success' => true,
+                'message' => "You've been added to the list."
+            ];
+        } catch (\Exception $e) {
+            $error = json_decode($e->getResponse()->getBody()->getContents(), true);
+            if ( !empty($error) ) { 
+                return [
+                    'success' => false,
+                    'message' => 'Error: ' . $error[0]['error_message']
+                ];
+            }
+            return [
+                'success' => false,
+                'message' => 'An unknown error occurred.'
+            ];
+        }
+        return null;
     }
 }
